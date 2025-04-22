183cfebf32c7359c57c780ffddcc07f8e0441d08
user <user@users.noreply.github.com>
2025-04-22 17:08:28 +1000

    loads editor | editor fails | buttons hide/show

--- resources/js/pages/Dashboard.vue
+++ resources/js/pages/Dashboard.vue
@@ -13,116 +13,116 @@ const breadcrumbs: BreadcrumbItem[] = [
     },
 ];
 
-const loading = ref(true);
-const beeInstance = ref<any>(null); 
+const isBuilderVisible = ref(false);
 
 const getBasicTemplate = () => ({
-  "page": {
-    "title": "",
-    "description": "",
-    "template": {
-      "name": "template-base",
-      "type": "basic",
-      "version": "0.0.1"
-    },
-    "body": {
-      "type": "mailup-bee-page-proprerties",
-      "container": {
-        "style": {
-          "background-color": "#FFFFFF"
-        }
-      },
-      "content": {
-        "style": {
-          "font-family": "Arial, 'Helvetica Neue', Helvetica, sans-serif",
-          "color": "#000000"
+    "page": {
+        "title": "",
+        "description": "",
+        "template": {
+            "name": "template-base",
+            "type": "basic",
+            "version": "0.0.1"
         },
-        "computedStyle": {
-          "linkColor": "#0068A5",
-          "messageBackgroundColor": "transparent",
-          "messageWidth": "500px"
-        }
-      }
-    },
-    "rows": [
-      {
-        "type": "one-column-empty",
-        "container": {
-          "style": {
-            "background-color": "transparent"
-          }
-        },
-        "content": {
-          "style": {
-            "background-color": "transparent",
-            "color": "#000000",
-            "width": "500px"
-          }
-        },
-        "columns": [
-          {
-            "grid-columns": 12,
-            "modules": [
-              {
-                "type": "mailup-bee-newsletter-modules-empty",
-                "descriptor": {}
-              }
-            ],
-            "style": {
-              "background-color": "transparent",
-              "padding-top": "5px",
-              "padding-right": "0px",
-              "padding-bottom": "5px",
-              "padding-left": "0px",
-              "border-top": "0px dotted transparent",
-              "border-right": "0px dotted transparent",
-              "border-bottom": "0px dotted transparent",
-              "border-left": "0px dotted transparent"
+        "body": {
+            "type": "mailup-bee-page-proprerties",
+            "container": {
+                "style": {
+                    "background-color": "#FFFFFF"
+                }
+            },
+            "content": {
+                "style": {
+                    "font-family": "Arial, 'Helvetica Neue', Helvetica, sans-serif",
+                    "color": "#000000"
+                },
+                "computedStyle": {
+                    "linkColor": "#0068A5",
+                    "messageBackgroundColor": "transparent",
+                    "messageWidth": "500px"
+                }
             }
-          }
+        },
+        "rows": [
+            {
+                "type": "one-column-empty",
+                "container": {
+                    "style": {
+                        "background-color": "transparent"
+                    }
+                },
+                "content": {
+                    "style": {
+                        "background-color": "transparent",
+                        "color": "#000000",
+                        "width": "500px"
+                    }
+                },
+                "columns": [
+                    {
+                        "grid-columns": 12,
+                        "modules": [
+                            {
+                                "type": "mailup-bee-newsletter-modules-empty",
+                                "descriptor": {}
+                            }
+                        ],
+                        "style": {
+                            "background-color": "transparent",
+                            "padding-top": "5px",
+                            "padding-right": "0px",
+                            "padding-bottom": "5px",
+                            "padding-left": "0px",
+                            "border-top": "0px dotted transparent",
+                            "border-right": "0px dotted transparent",
+                            "border-bottom": "0px dotted transparent",
+                            "border-left": "0px dotted transparent"
+                        }
+                    }
+                ]
+            }
         ]
-      }
-    ]
-  }
+    }
 });
 
-onMounted(async () => {
+async function showBuilder() {
+    console.log('button clicked');
     try {
+        isBuilderVisible.value = true;
+
+        var bee;
         const response = await axios.post('/beefree/token');
         const token = response.data.token;
         console.log('Token received:', token);
 
-        const beeConfig = {
+        const config = {
             uid: 'test1-clientside',
             container: 'bee-plugin-container',
         };
 
-        beeInstance.value = new BeefreeSDK(token);
+        window.BeePlugin.create(token, config, function(instance) {
+            bee = instance;
+            console.log('loaded');
+        });
 
-        const template = getBasicTemplate();
-        console.log('Using template:', template);
-
-        await beeInstance.value.start(beeConfig, template);
-
-        console.log('Beefree SDK initialized successfully');
-        loading.value = false;
     } catch (error) {
         console.error('Failed to initialize Beefree SDK:', {
             status: error.response?.status,
             data: error.response?.data,
             fullError: error
         });
-        loading.value = false;
     }
-});
+}
 </script>
 
 <template>
     <Head title="Dashboard" />
     <AppLayout :breadcrumbs="breadcrumbs">
         <div class="flex h-full flex-1 flex-col gap-4 rounded-xl p-4">
-            <div v-if="loading">Loading editor...</div>
-            <div v-else id="bee-plugin-container" class="w-full h-[800px] border border-gray-300"></div>
+            <div v-if="isBuilderVisible" id="bee-plugin-container" class="w-full h-[800px] border bg-gray-900"></div>
+            <div v-if="!isBuilderVisible">
+                <button @click="showBuilder">click</button>
+            </div>
         </div>
     </AppLayout>
 </template>
--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -34,6 +34,7 @@
 
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
+        <script src="https://app-rsrc.getbee.io/plugin/BeePlugin.js"></script>
 
         @routes
         @vite(['resources/js/app.ts'])
